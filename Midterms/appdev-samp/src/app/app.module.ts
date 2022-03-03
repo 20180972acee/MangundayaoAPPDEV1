ffbf3ac544d1d48c6e5129afa6be1741755a8050
20180972 <20180972@s.ubaguio.edu>
2022-03-03 13:33:31 +0800

    components added

--- Midterms/appdev-samp/src/app/app.module.ts
+++ Midterms/appdev-samp/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { NaviComponent } from './navi/navi.component';
+import { CourseInfoComponent } from './course-info/course-info.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ContactsInfoComponent } from './contacts-info/contacts-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    NaviComponent,
+    CourseInfoComponent,
+    ArticlesComponent,
+    ContactsInfoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,
+  CourseListComponent]
 })
 export class AppModule { }
